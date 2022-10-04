541b7307a290671a0efcce448d4c591ed84fe829
Seibel <asherisrad@gmail.com>
2022-10-04 12:38:26 -0700

    Signed-off-by: Seibel <asherisrad@gmail.com>

---
+++ Assets/Racket.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    public float speed = 150;
+
+    void FixedUpdate () {
+        float h = Input.GetAxisRaw("Horizontal");
+
+    // Set Velocity (movement direction * speed)
+    GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
